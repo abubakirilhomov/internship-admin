1b5f17ce1c94b203e4daeb1158faa6de21307609
abubakirilhomov <2ilhomovabubakir2@gmail.com>
2025-08-23 14:53:08 +0500

    asdsadsa

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL1;
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 console.log(API_BASE_URL)
 const user = JSON.parse(localStorage.getItem("user"));
 const getAuthHeaders = () => {
